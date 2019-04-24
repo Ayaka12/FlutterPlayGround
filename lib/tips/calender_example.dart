6c6568a533aa89ef6454e6616c4649d6ed518273
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2019-04-24 17:27:53 +0900

    refactor

--- lib/tips/calender_example.dart
+++ lib/tips/calender_example.dart
@@ -3,6 +3,8 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel;
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/classes/event_list.dart';
 
 class CalenderExample extends StatefulWidget {
   @override
@@ -13,10 +15,37 @@ class CalenderExample extends StatefulWidget {
 
 class _CalenderExampleState extends State<CalenderExample> {
   DateTime _currentDate = DateTime.now();
+  EventList<Event> _markedDateMap = EventList<Event>();
 
   void onDayPressed(DateTime date, List<Event> events) {
     this.setState(() => _currentDate = date);
-    Fluttertoast.showToast(msg: date.toString());
+    if (events.length == 0) {
+      Fluttertoast.showToast(msg: date.toString());
+      addEvent(date);
+    } else {
+      Fluttertoast.showToast(msg: events[0].title);
+    }
+    // addEvent(date);
+  }
+
+  void addEvent(DateTime date) {
+    _markedDateMap.add(date, createEvent(date));
+  }
+
+  Event createEvent(DateTime date) {
+    return Event(
+        date: date,
+        title: date.day.toString(),
+        icon: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.blue, width: 1.0)),
+            child: Icon(
+              Icons.calendar_today,
+              color: Colors.blue,
+            )
+        )
+    );
   }
 
   @override
@@ -35,6 +64,7 @@ class _CalenderExampleState extends State<CalenderExample> {
               selectedDateTime: _currentDate,
               daysHaveCircularBorder: false,
               customGridViewPhysics: NeverScrollableScrollPhysics(),
+              markedDatesMap: _markedDateMap,
               markedDateShowIcon: true,
               markedDateIconMaxShown: 2,
               todayTextStyle: TextStyle(
